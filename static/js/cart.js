9412548e4f84dfafaf3566998e70e333923653a2
mksahota10 <mksahota10@gmail.com>
2021-02-15 01:37:09 -0800

    updated add to cart button

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,37 +1,38 @@
 let updateBtns = document.getElementsByClassName('update-cart')
 
-for(i=0; i<updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        let productId = this.dataset.product
-        let action = this.dataset.action 
-        console.log('productId:', productId, 'action:', action)
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		let productId = this.dataset.product
+		let action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
 
-        console.log('USER:', user)
-        if (user == 'AnonymousUser'){
-            console.log('User is not authenticated')
-        }else{
-            updateUserOrder(prodictId, action)
-        }
-    })
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action)
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
+
 function updateUserOrder(productId, action){
-    console.log('User is authenticated, sending data...')
+	console.log('User is authenticated, sending data...')
 
-    let url = '/update_item/'
+		let url = '/update_item/'
 
-    fetch(url, {
-        method: "POST", 
-        headers:{
-            'Content-Type': 'application/json',
-            "X-CSRFToken": csrftoken, 
-        },
-        body:JSON.stringify({'productId': productId, 'action': action})
-    })
-    .then((response)=>{
-        return response.json(); 
-    })
-    .then((data)=>{
-       location.reload()
-    })
-
+		fetch(url, {
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'X-CSRFToken':csrftoken,
+			}, 
+			body:JSON.stringify({'productId':productId, 'action':action})
+		})
+		.then((response) => {
+		   return response.json();
+		})
+		.then((data) => {
+		    location.reload()
+		});
 }
+
