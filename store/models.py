928f8b5ccfea249b78a6d430f9cc4a283e048b7a
mksahota10 <mksahota10@gmail.com>
2021-02-14 23:18:36 -0800

    all items now rendering

--- store/models.py
+++ store/models.py
@@ -12,21 +12,42 @@ class Customer(models.Model):
 		return self.name
 
 class Product(models.Model):
-    email = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
-    digital = models.BooleanField(default=Flase, null=True, blank=False)
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
-    def__str__(self):
-        return self.name
-
+    def __str__(self):
+       return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url 
+        except:
+            url = ''
+        return url
+        
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
-    def__str__(self):
+    def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 # items to order with many to one relationship 
 #cart can have multiple relationships
@@ -34,10 +55,15 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    order = models.ForeignKey(ORDER, on_delte=moedels.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
-    date_added = models.DataTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 
