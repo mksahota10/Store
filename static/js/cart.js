fca6358d7b4257d030844539ffde93246e8b8a27
mksahota10 <mksahota10@gmail.com>
2021-02-15 00:15:11 -0800

    added buttons and auth

---
+++ static/js/cart.js
@@ -0,0 +1,19 @@
+let updateBtns = document.getElementsByClassName('update-cart')
+
+for(i=0; i<updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        let productId = this.dataset.product
+        let action = this.dataset.action 
+        console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:', user)
+        if (user == 'AnonymousUser'){
+            console.log('User is not authenticated')
+        }else{
+            updateUserOrder(prodictId, action)
+        }
+    })
+}
+function updateUserOrder(productId, action){
+    console.log('User is authenticated, sending data...')
+}
--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+
 
 # Create your views here.
 def store(request):
@@ -29,3 +31,5 @@ def checkout(request):
      context = {"items":items, 'order':order}
      return render(request, 'store/checkout.html', context)
 
+def updateItem(request):
+     return JsonResponse('Item was added', safe=False)
