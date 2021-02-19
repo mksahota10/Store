6afa052939912c870c4776677ffce88650856a87
mksahota10 <mksahota10@gmail.com>
2021-02-19 11:25:57 -0800

    css updated

--- store/urls.py
+++ store/urls.py
@@ -1,21 +1,15 @@
 
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
 
 from . import views
 
 urlpatterns = [
-        #Leave as empty string for base url
+	#Leave as empty string for base url
 	path('', views.store, name="store"),
 	path('cart/', views.cart, name="cart"),
 	path('checkout/', views.checkout, name="checkout"),
+
 	path('update_item/', views.updateItem, name="update_item"),
 	path('process_order/', views.processOrder, name="process_order"),
 
-	# path('register/', views.registerPage, name="register"),
-	# path('login/', views.loginPage, name="login"),  
-	# path('logout/', views.logoutUser, name="logout"),
-
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
