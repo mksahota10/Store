1952c651d0ed08ee0650a94a975bb9f1cdec6bb3
mksahota10 <mksahota10@gmail.com>
2021-02-18 18:30:18 -0800

    updates made to add cart form

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -15,3 +17,4 @@ urlpatterns = [
 	# path('logout/', views.logoutUser, name="logout"),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/utils.py
+++ store/utils.py
@@ -81,3 +81,31 @@ def guestOrder(request, data):
 			quantity=item['quantity'],
 		)
 	return customer, order
+
+	
+def guestOrder(request, data):
+	name = data['form']['name']
+	email = data['form']['email']
+
+	cookieData = cookieCart(request)
+	items = cookieData['items']
+
+	customer, created = Customer.objects.get_or_create(
+			email=email,
+			)
+	customer.name = name
+	customer.save()
+
+	order = Order.objects.create(
+		customer=customer,
+		complete=False,
+		)
+
+	for item in items:
+		product = Product.objects.get(id=item['id'])
+		orderItem = OrderItem.objects.create(
+			product=product,
+			order=order,
+			quantity=item['quantity'],
+		)
+	return customer, order
